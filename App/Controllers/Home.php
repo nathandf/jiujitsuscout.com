cfe9dd3cb15d24424685345dcbd17fe3d0a0a278
JiuJitsuScout <jiujitsuscout@gmail.com>
2018-07-07 21:17:27 -0500

    Test error on geo ip

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -14,6 +14,7 @@ class Home extends Controller
         // Get geo info of user by ip using the IPInfo API
         $geoIP = $ipinfo->getGeoByIP();
 
+        vdumpd($geoIP);
         $this->view->assign( "geo", $geoIP );
     }
 
