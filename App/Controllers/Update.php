0a0b25a064017b8227ac3ad145c9c1ac488a3b41
JiuJitsuScout <jiujitsuscout@gmail.com>
2018-07-15 18:42:47 -0500

    Update code formatting from spaces to tags in ReviewRepository. Add function to transfer data from ratings and reviews from the old DB to the new.

--- App/Controllers/Update.php
+++ App/Controllers/Update.php
@@ -7,34 +7,48 @@ use \Core\Controller;
 class Update extends Controller
 {
 
-    public function leadDataTransferAction()
+    public function partnerDataTransferAction()
     {
         set_time_limit(600);
-        // $prospectRegistrar = $this->load( "prospect-registrar" );
-        // $prospectRepo = $this->load( "prospect-repository" );
-        // $phoneRepo = $this->load( "phone-repository" );
-        // $businessRepo = $this->load( "business-repository" );
-        //
-        // $dm = new \Models\Mappers\DataMapperTEMP($this->container);
-        //
-        // $leads = $dm->getAll( "leads" );
-        // $businesses = $businessRepo->getAll();
-        //
-        // foreach ( $businesses as $business ) {
-        //     foreach ( $leads as $lead ) {
-        //         if ( $lead[ "gym_name" ] == $business->business_name ) {
-        //             $prospect = $prospectRegistrar->build();
-        //             $prospect->business_id = $business->id;
-        //             $prospect->first_name = $lead[ "name" ];
-        //             $prospect->last_name = "";
-        //             $prospect->email = $lead[ "email" ];
-        //             $prospect->source = $lead[ "source" ];
-        //             $phone = $phoneRepo->create( 1, preg_replace( "/[^0-9,.]/", "", $lead[ "phone_number" ] ) );
-        //             $prospect->phone_id = $phone->id;
-        //             $prospectRegistrar->register( $prospect );
-        //         }
-        //     }
-        // }
+        $prospectRegistrar = $this->load( "prospect-registrar" );
+        $prospectRepo = $this->load( "prospect-repository" );
+        $phoneRepo = $this->load( "phone-repository" );
+        $businessRepo = $this->load( "business-repository" );
+        $reviewRepo = $this->load( "review-repository" );
+
+        $dm = new \Models\Mappers\DataMapperTEMP($this->container);
+
+        $ratings_reviews = $dm->getAll( "ratings_reviews" );
+        #$leads = $dm->getAll( "leads" );
+        $businesses = $businessRepo->getAll();
+
+        foreach ( $businesses as $business ) {
+            foreach ( $ratings_reviews as $rr ) {
+                if ( $rr[ "gym_name" ] == $business->business_name ) {
+                    $reviewRepo->create(
+                        $business->id,
+                        $rr[ "name" ],
+                        $rr[ "email" ],
+                        $rr[ "review" ],
+                        $rr[ "rating" ],
+                        strtotime( $rr[ "timestamp" ] )
+                    );
+                }
+            }
+            // foreach ( $leads as $lead ) {
+            //     if ( $lead[ "gym_name" ] == $business->business_name ) {
+            //         $prospect = $prospectRegistrar->build();
+            //         $prospect->business_id = $business->id;
+            //         $prospect->first_name = $lead[ "name" ];
+            //         $prospect->last_name = "";
+            //         $prospect->email = $lead[ "email" ];
+            //         $prospect->source = $lead[ "source" ];
+            //         $phone = $phoneRepo->create( 1, preg_replace( "/[^0-9,.]/", "", $lead[ "phone_number" ] ) );
+            //         $prospect->phone_id = $phone->id;
+            //         $prospectRegistrar->register( $prospect );
+            //     }
+            // }
+        }
     }
 
   public function databaseAction()
