952e82f4732a6bb3ef679693886f89267cd77950
JiuJitsuScout <jiujitsuscout@gmail.com>
2019-03-20 11:49:01 -0500

    database updates

--- production-updates.sql
+++
@@ -1,2 +0,0 @@
-
-CREATE TABLE `landing_page_sign_up` ( `id` BIGINT NOT NULL AUTO_INCREMENT , `landing_page_id` BIGINT NOT NULL , `token` VARCHAR(512) NOT NULL , PRIMARY KEY (`id`)) engine = InnoDB;
